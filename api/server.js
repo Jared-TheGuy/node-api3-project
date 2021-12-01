c609da6344c5ccb16a29222f96a44633521e2976
Jared Bates-Baker <51772046+Jared-TheGuy@users.noreply.github.com>
2021-12-01 13:21:29 -0700

    created a global Logger middleware

--- api/server.js
+++ api/server.js
@@ -3,10 +3,24 @@ const express = require('express');
 const server = express();
 const router = require('./users/users-router');
 
-server.use('/api/users', router)
+
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
+
+//logger
+server.use((req, res, next) => {
+
+  console.log(
+    req.method,
+    req.url,
+    Date.now() 
+    );
+  
+  next();
+})
+
+server.use('/api/users', router)
 
 // global middlewares and the user's router need to be connected here
 
